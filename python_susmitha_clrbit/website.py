34453ccdc1364927b51d8b5b5f4ccb4b6460fb32
Sandeep Cherukuri <sandeep.cherukuri@sapientrazorfish.com>
2020-11-21 21:27:45 -0600

    removed unused imports and code.

--- python_susmitha_clrbit/website.py
+++ python_susmitha_clrbit/website.py
@@ -1,42 +1,20 @@
 from __future__ import print_function
+
 import os
-import webbrowser
-import ssl
-import re
-import requests
-from urllib.parse import urlsplit
-from collections import deque
-from bs4 import BeautifulSoup
-import pandas as pd
-import operator
-from google.colab import files
-
-import time
-import pickle
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import NoSuchElementException
-
-import pickle
 import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-import requests
-import json
+import pickle
 import re
-from urllib.parse import urlsplit
-from collections import deque
-from bs4 import BeautifulSoup
-import pandas as pd
-from google.colab import files
+import time
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SAMPLE_SPREADSHEET_ID = '1meboQWv-2OMnM328b2BGGseG9bUcR6KZ9PeMTgVBMI0' #chsandeep511@gmail.com
+SAMPLE_SPREADSHEET_ID = '1meboQWv-2OMnM328b2BGGseG9bUcR6KZ9PeMTgVBMI0'  # chsandeep511@gmail.com
 
 searchDomain_ = []
 overQuota = []
@@ -69,33 +47,37 @@ def initializeSheets():
     sheet = service.spreadsheets()
     return sheet
 
+
 def readSheets(sheet):
-    #TODO:// Update with your Sheet number and from `where to where` you want to read the domian list: Susmitha
+    # TODO:// Update with your Sheet number and from `where to where` you want to read the domian list: Susmitha
     # The A1 notation of the values to update.
     # start with A2 always
-    read_range_ = 'Sheet10!P158:AA160' #Should be same Row number at line 60: Here I am reading at Column 'A' 10th Row to 20th row
+    read_range_ = 'Sheet10!P158:AA160'  # Should be same Row number at line 60: Here I am reading at Column 'A' 10th Row to 20th row
 
     result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=read_range_).execute()
     values = result.get('values', [])
     return values
 
+
 def valid_email(email):
-  return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+    return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+
 
 def openChromeDriver(to_, subject_, composebody_):
     chromedriver = "/Users/sancheru/Downloads/chromedriver"
 
     chrome_options = Options()
     chrome_options.add_argument("--user-data-dir=chrome-data")
-    driver = webdriver.Chrome(chromedriver)#,options=chrome_options)
-    driver.get('https://caprica.seoboxes.com:2096/cpsess8220829249/3rdparty/roundcube/?_task=mail&_action=compose&_id=14557673775fb5a90c1c498')
-
+    driver = webdriver.Chrome(chromedriver)  # ,options=chrome_options)
+    driver.get(
+        'https://caprica.seoboxes.com:2096/cpsess8220829249/3rdparty/roundcube/?_task=mail&_action=compose&_id=14557673775fb5a90c1c498')
 
     username_input = '//*[@id="user"]'
     password_input = '//*[@id="pass"]'
     login_submit = '//*[@id="login_submit"]'
 
-    driver.find_element_by_xpath(username_input).send_keys("stewardsmike@trafficluxury.com")#stewardsmike@trafficluxury.com
+    driver.find_element_by_xpath(username_input).send_keys(
+        "stewardsmike@trafficluxury.com")  # stewardsmike@trafficluxury.com
     driver.find_element_by_xpath(password_input).send_keys("stewardsmike34")
     driver.find_element_by_xpath(login_submit).click()
     driver.maximize_window()
@@ -108,13 +90,13 @@ def openChromeDriver(to_, subject_, composebody_):
 
     to_input = '//*[@id="_to"]'
     subject_input = '//*[@id="compose-subject"]'
-    composebody_input = '//*[@id="composebody"]'
+    composed_input = '//*[@id="composebody"]'
     send_message_submit = '//*[@id="rcmbtn107"]'
     resend_message_submit = '/html/body/div[8]/div[3]/div/button[1]'
 
     driver.find_element_by_xpath(to_input).send_keys(to_)
     driver.find_element_by_xpath(subject_input).send_keys(subject_)
-    driver.find_element_by_xpath(composebody_input).send_keys(composebody_)
+    driver.find_element_by_xpath(composed_input).send_keys(composebody_)
 
     time.sleep(1)
     driver.find_element_by_xpath(send_message_submit).click()
@@ -127,6 +109,7 @@ def openChromeDriver(to_, subject_, composebody_):
 
     driver.quit()
     print("quit.")
+
 
 def main():
     # [START sheets_get_values]
@@ -146,14 +129,14 @@ def main():
             else:
                 searchDomain_.append(row[0])
                 print("Email: %s" % row[3])
-                #print(item, row[3]) #row[0]--> Subject;; row[1]--> composebody;; row[3]--> sender_to_
-                if row[3].find("Not Found,,") != -1 or row[3].find("Error,Error,") != -1 or row[3].find("Error,,") != -1 or row[3].find("Error,,Error") != -1:
+                # print(item, row[3]) #row[0]--> Subject;; row[1]--> composed;; row[3]--> sender_to_
+                if row[3].find("Not Found,,") != -1 or row[3].find("Error,Error,") != -1 or row[3].find(
+                        "Error,,") != -1 or row[3].find("Error,,Error") != -1:
                     print("ignore")
-                else :
-                    #print(row[3])
+                else:
+                    # print(row[3])
                     openChromeDriver(row[3], row[0], row[1])
-                    #print(result)
-
+                    # print(result)
 
 
 if __name__ == '__main__':
